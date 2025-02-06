aedf40fe7d17f3c2b4dd67253f654157cf89409d
RonghaiHe <hrhkjys@qq.com>
2025-02-06 10:01:40 +0800

    fix: update git repository URLs to use SSH format in CMake files

--- cmake/roptlib.cmake
+++ cmake/roptlib.cmake
@@ -4,7 +4,7 @@ project(roptlib-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(roptlib
-        GIT_REPOSITORY    https://github.com/yuluntian/ROPTLIB.git
+        GIT_REPOSITORY    git@github.com:yuluntian/ROPTLIB.git
         GIT_TAG           feature/cmake
         SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/roptlib-src"
         BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/roptlib-build"
